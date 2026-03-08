be854c11506ac37255b0f0e248398296121cb011
Heath Stamper <cavemanheath@aol.com>
2026-03-07 19:12:16 -0600

    Create arduino_firmware.ino

---
+++ docs/hardware/software/software/arduino_firmware.ino
@@ -0,0 +1,8 @@
+// MA-INS-Open firmware stub
+void setup() {
+  Serial.begin(115200);
+  // frequency counter on TPBR signal + Kalman update here
+}
+void loop() {
+  // read phase, update position estimate, print to Serial
+}
